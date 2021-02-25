dd255fc79f3d9837e57fe200c2be8668256e690e
jieran233 <63990576+jieran233@users.noreply.github.com>
2021-02-25 20:45:14 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 :: 该批处理脚本用于在Windows平台的服务器上运行闻言并输出日志到run.log
 :: 请使用任务计划程序定期运行本批处理脚本
-echo [%date% %time%] >> run.log
-python main.py >> run.log
-echo. >> run.log
+:: 请把相对路径替换为绝对路径，以确保任务计划能够万无一失顺利执行
+echo [%date% %time%] >> run.log && python main.py >> run.log
